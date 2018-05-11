1c6c940654241ec54b2a5de8472a4fac99f68183
gsm001 <m13912283052@163.com>
2018-05-11 18:50:34 +0800

    首页修改更新完成

--- public/back2/js/index.js
+++ public/back2/js/index.js
@@ -1,7 +1,13 @@
-$(function(){
-// 基于准备好的dom，初始化echarts实例
-  var echarts1 = echarts.init(document.querySelector('.echarts_1'));
-  
+/**
+ * Created by Jepson on 2018/5/11.
+ */
+
+$(function() {
+
+  // 1. 实现柱状图
+  // 基于准备好的dom，初始化echarts实例
+  var echarts_1 = echarts.init(document.querySelector(".echarts_1"));
+
   // 指定图表的配置项和数据
   var option1 = {
     // 大标题
@@ -29,15 +35,14 @@ $(function(){
       data: [1000, 1500, 1800, 2000, 2500, 1300]
     }]
   };
-  
+
   // 使用刚指定的配置项和数据显示图表。
-  echarts1.setOption(option1);
-  
-  
-  
+  echarts_1.setOption(option1);
+
+
   // 2. 实现饼状图
-  var echarts2 = echarts.init(document.querySelector('.echarts_2'));
-  
+  var echarts_2 = echarts.init(document.querySelector(".echarts_2"));
+
   // 指定图表的配置项和数据
   var option2 = {
     title : {
@@ -53,11 +58,12 @@ $(function(){
       // {a}（系列名称），{b}（数据项名称），{c}（数值）, {d}（百分比）
       formatter: "{a} <br/>{b} : {c} ({d}%)"
     },
+    // 图例
     legend: {
       // 设置图例的排列方式
       orient: 'vertical',
       left: 'left',
-      data: ['耐克','阿迪','新百伦','李宁','阿迪王']
+      data: ['耐克','阿迪王','新百伦','李宁','阿迪']
     },
     series : [
       {
@@ -69,15 +75,15 @@ $(function(){
         center: ['50%', '60%'],
         data:[
           {value:335, name:'耐克'},
-          {value:310, name:'阿迪'},
+          {value:310, name:'阿迪王'},
           {value:234, name:'新百伦'},
           {value:135, name:'李宁'},
-          {value:1548, name:'阿迪王'}
+          {value:1548, name:'阿迪'}
         ],
         itemStyle: {
           // 阴影效果
           emphasis: {
-            shadowBlur: 10,
+            shadowBlur: 100,
             shadowOffsetX: 0,
             shadowColor: 'rgba(0, 0, 0, 0.5)'
           }
@@ -85,7 +91,8 @@ $(function(){
       }
     ]
   };
-  
+
   // 使用刚指定的配置项和数据显示图表。
-  echarts2.setOption(option2);
+  echarts_2.setOption(option2);
+
 })
